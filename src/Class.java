339decf9664ada41e19f4f34df774fca7f6791c9
Ivan Danilyuk <werwetsad@gmail.com>
2022-12-18 22:19:57 +0500

    Refactored all tests

---
+++ src/Class.java
@@ -0,0 +1,10 @@
+
+public class Class {
+    public static void main(String[] args) {
+        String l = String.valueOf(Integer.MIN_VALUE);
+        System.out.print(Class.Decode("015"));
+    }
+    public static Integer Decode(String nm){
+        return Integer.decode(nm);
+    }
+}
